108cf263326abd142c3d9cff6ebb9fce4c36a804
snehal <snehalreet199@gmail.com>
2020-12-15 15:11:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//Connect database
+connectDB();
+
+const PORT = process.env.PORT  || 5000;
+
+app.listen(PORT , () => console.log(`Server started on port ${PORT}`));
